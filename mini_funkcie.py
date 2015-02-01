d281a3aa7305386dcc360eeb585e6f95b8986c3d
dlitvik <dusan.yolo@gmail.com>
2015-02-01 13:50:56 +0100

    mini funkcie
    learn python

---
+++ mini_funkcie.py
@@ -0,0 +1,10 @@
+def vytvor_list_and_tuple():
+    """nacita sekvenciu cisel oddelenzch ciarkami a vytvori list
+        a tuple"""
+    values = input()
+    l = values.split(",")
+    t = tuple(l)
+    print (l)
+    print(t)
+
+def simple_class():
