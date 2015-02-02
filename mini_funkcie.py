2d50d784a31f4ec5ea745a829a64be1bfb99ed9d
dlitvik <dusan.yolo@gmail.com>
2015-02-02 10:41:35 +0100

    nova funkcia
    pridanie novej funkcie

--- mini_funkcie.py
+++ mini_funkcie.py
@@ -8,4 +8,14 @@ def vytvor_list_and_tuple():
     print (l)
     print(t)
 
-def simple_class():
+def square_root():
+    """vypocita hodnotu pre zadanu sekvenciu cisel"""
+    import math
+    c = 50
+    h = 30
+    values = []
+    d = input().split(",")
+    for x in d:
+        q = int(math.sqrt((2*c*int(x))/h))
+        values.append(q)
+    print(values)
